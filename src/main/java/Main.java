da630117e6580a0ea4ffdb0110334d1c1046cb62
uncho <uncho.tnct@gmail.com>
2019-10-10 20:53:44 +0900

    add source

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]){
+        int variable = 1;
+        variable = 2;
+
+        if (true && variable == 1) {
+            System.out.println(variable);
+        }
+    }
+}
