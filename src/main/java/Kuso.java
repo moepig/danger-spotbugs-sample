aa5cebd4852f740825e5cc101e84353479c55a4b
uncho <uncho.tnct@gmail.com>
2019-10-13 00:58:41 +0900

    クソ実装のクラスを追加

---
+++ src/main/java/Kuso.java
@@ -0,0 +1,22 @@
+public class Kuso {
+
+    public Kuso() {
+        if (false) {
+            System.out.println("toilet");
+        }
+    }
+
+    public int unko() {
+        String ben = null;
+
+        return Integer.getInteger(ben);
+    }
+
+    public class KusoInner {
+        public int baka;
+
+        public int getBaka() {
+            return baka;
+        }
+    }
+}
